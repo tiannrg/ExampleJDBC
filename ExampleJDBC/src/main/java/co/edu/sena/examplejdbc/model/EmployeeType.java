ead61fc91c5b872348172d7767bac69a618cd0be
Sebastian Rojas Gonzalez <rojasgonzales0714@gmail.com>
2025-03-13 12:25:30 -0500

    DBEmploye
    Se  codifica la clase de conexion a la tabla employee con sus respectivas test

---
+++ ExampleJDBC/src/main/java/co/edu/sena/examplejdbc/model/EmployeeType.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.sena.examplejdbc.model;
+
+/**
+ *
+ * @author rojas
+ */
+public class EmployeeType {
+    private int id;
+    private String descript;
+
+    public EmployeeType() {
+    }
+
+    public EmployeeType(int id, String descript) {
+        this.id = id;
+        this.descript = descript;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescript() {
+        return descript;
+    }
+
+    public void setDescript(String descript) {
+        this.descript = descript;
+    }
+    
+    
+}
