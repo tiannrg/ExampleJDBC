6d1744b0f94432e20b6af33d96fcf098bfd8aa48
Sebastian Rojas Gonzalez <rojasgonzales0714@gmail.com>
2025-03-13 08:21:35 -0500

    Configuracion Proyecto
    Se añade la dependencia de MySql conector j al pomx y se agregan tags de documentacion

---
+++ ExampleJDBC/src/main/java/co/edu/sena/examplejdbc/ExampleJDBC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package co.edu.sena.examplejdbc;
+
+/**
+ *
+ * @author rojas
+ */
+public class ExampleJDBC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
