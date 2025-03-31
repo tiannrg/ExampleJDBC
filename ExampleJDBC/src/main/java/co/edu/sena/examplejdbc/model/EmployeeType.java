b75fb80f746a9c7e6186f1bbeb859b3512ff3e05
Sebastian Rojas Gonzalez <rojasgonzales0714@gmail.com>
2025-03-30 23:42:35 -0500

    View EmployeeType y Employee
    Se crea y codifica el frame completo para tipo de empleado y empleado

--- ExampleJDBC/src/main/java/co/edu/sena/examplejdbc/model/EmployeeType.java
+++ ExampleJDBC/src/main/java/co/edu/sena/examplejdbc/model/EmployeeType.java
@@ -35,6 +35,11 @@ public class EmployeeType {
     public void setDescript(String descript) {
         this.descript = descript;
     }
+
+    @Override
+    public String toString() {
+        return descript;
+    }
     
     
 }
